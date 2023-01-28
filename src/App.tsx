9574b01063aa1bc793f8bdc23f64687f22c74352
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2023-01-28 15:26:31 -0300

    Adiciona thedogapi.

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,16 @@ import { withLoader } from "./components/hoc/WithLoader/with-loader";
 import ThemeSwitcher from "./components/provide/ThemeSwitcher";
 import { ThemeContext } from "./context/theme-context";
 
-async function fetchCats(): Promise<CatImage[]> {
-  const catApiBaseUrl = "https://api.thecatapi.com/v1";
-  const catApiImagesUrl = `${catApiBaseUrl}/images/search?limit=10`;
+async function fetchDogs(): Promise<CatImage[]> {
+  const dogApiBaseUrl = "https://api.thedogapi.com/v1";
+  const dogApiImagesUrl = `${dogApiBaseUrl}/images/search?limit=10`;
 
   // OBS: fetch simples sem tratamento de erro.
-  const catImgs: CatImage[] = await (await fetch(catApiImagesUrl)).json();
-  return catImgs;
+  const dogImgs: CatImage[] = await (await fetch(dogApiImagesUrl)).json();
+  return dogImgs;
 }
 
-// const CatsRenderWithLoader = withLoader(CatsRender, fetchCats);
-const CatsRenderWithLoader = withLoader(fetchCats)(CatsRender);
+const CatsRenderWithLoader = withLoader(fetchDogs)(CatsRender);
 
 function App() {
   const { theme, colorScheme } = useContext(ThemeContext);
@@ -35,10 +34,10 @@ function App() {
       <CatsContainer />
 
       <h3>WithLoader</h3>
-      <WithLoader fetchFunction={fetchCats}>
+      <WithLoader fetchFunction={fetchDogs}>
         {(fd) => (
           <>
-            asas {console.debug(fd)} <CatsRender fetchedData={fd} />
+            asas {/*console.debug(fd)*/} <CatsRender fetchedData={fd} />
           </>
         )}
       </WithLoader>
